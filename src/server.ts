4e9c5b02996687cc4d2514a52ae1418fbe23957b
thrialectics <203729272+thrialectics@users.noreply.github.com>
2026-02-04 18:43:28 -0500

    Updated for server-side game state handling with Vite-Express

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import { createGame, makeMove } from "./tic-tac-toe";
+
+const app = express();
+let gameState = createGame()
+
+app.use(express.json())
+
+app.post("/api/reset", (_req, res) => {
+    gameState = createGame();
+    res.json(gameState);
+});
+
+app.post("/api/move", (req, res) => {
+    const position = req.body.position;
+    gameState = makeMove(gameState, position);
+    res.json(gameState);
+});
+
+app.get("/api/game", (_req, res) => {
+    res.json(gameState);
+});
+
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
